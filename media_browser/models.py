4c11d062e5aa51800efb13d875ae131415ac7e9f
Brandon Saunders <brandon@vantageled.com>
2016-07-18 09:54:14 -0700

    Separating the video/audio templates. Scanning for video files. Reading video metadata

--- media_browser/models.py
+++ media_browser/models.py
@@ -24,12 +24,10 @@ class AudioTrack(models.Model):
 class VideoTrack(models.Model):
 	video_title = models.CharField(max_length=50, null=True)
 	artist = models.CharField(max_length=100, null=True)
-	date_added = models.DateField()
+	date_added = models.DateField(max_length=20, null=True)
 	genre = models.CharField(max_length=50, null=True)
 	play_count = models.IntegerField(null=True)
 	rating = models.CharField(max_length=10, null=True)
-	year = models.IntegerField(null=True)
-	file_size = models.DecimalField(max_digits=20, decimal_places=5, null=True)
-
-	def __str__(self):
-		return u'%s %s %s %s %s %s %s %s %s %s' % (self.song_title, self.artist, self.album, self.date_added, self.genre, self.play_count, self.rating, self.year, self.track_number, self.file_size)
+	year = models.CharField(max_length=20, null=True)
+	file_size = models.CharField(max_length=20, null=True)
+	file_name = models.CharField(max_length=20, null=True)
--- media_player/views.py
+++ media_player/views.py
@@ -4,13 +4,15 @@ from django.http import HttpResponse
 from django.core.files import File
 from django.core.files.storage import FileSystemStorage
 from django.db import models
-from media_browser.models import AudioTrack
+from media_browser.models import AudioTrack, VideoTrack
 from django.shortcuts import get_object_or_404
 
 import datetime
 import os
 import id3reader
 import string
+import subprocess
+
 
 VERBOSE = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -51,30 +53,9 @@ def trailers(request):
 
 	return HttpResponse(html)	
 
-def obtain_all_video_filenames():
-	''' This function will obtain a list of all video files in the media folder. '''
-	print("----------------------------------------------------------------")
-	print("Obtaining all video file_names")
-	if(VERBOSE):
-		print("\nMEDIA DIRECTORY: " + MEDIA_ROOT)
-
-	loaded_files = []
-	num_of_files = 0
-
-	# Iterate over all files found in the media directory. Count the total number of files.
-	for filename in os.listdir(os.getcwd() + "/media/"):
-		if(VERBOSE):
-			print(filename)
-		if(filename.endswith(".mp4") or filename.endswith(".ogg") or filename.endswith(".webm") or filename.endswith(".ogv") or filename.endswith(".mkv")):
-			num_of_files+=1
-			loaded_files.append(filename)
-
-	return loaded_files, num_of_files
-
-
 def audio_media_browser(request):
 	''' This function will render a media browser with all files in the media folder. '''
-	t = get_template('media_browser.html')
+	t = get_template('audio_media_browser.html')
 
 	# Provides a list of file_names.
 	found_files = obtain_all_audio_filenames()
@@ -102,28 +83,41 @@ def audio_media_browser(request):
 
 def video_media_browser(request):
 	''' This function will render a media browser with all files in the media folder. '''
-	t = get_template('media_browser.html')
+	t = get_template('video_media_browser.html')
 
 	# Provides a list of file_names.
-	found_files = obtain_all_video_filenames()
+	found_files, num_of_files = obtain_all_video_filenames()
 
-	# Checks the found files for 
-	#new_files = scan_for_new_audio_files()
-	'''
+	new_files = scan_for_new_video_files()
+
+	print(found_files)
+	print(new_files)
+	
+	
 	if(len(new_files) > 0):
 		print("New files were found.")
 		# Update the database with all the current media files found in the media folder.
- 		for new_file in new_files:
- 			print("Adding " + new_file)
- 			#write_ID3_tag_information_to_database(new_file)
+		print(new_files)
+ 		for new_file in found_files:
+ 			print(getVideoMetadata(str(new_file)))
+ 			print("Writing out video files")
+ 			print(new_file)
+ 			write_video_information_to_database(new_file)
 
 	else:
 		print("No new files.")
-		
-	audio_track_list = AudioTrack.objects.all()
-	'''
+
+	video_track_list = VideoTrack.objects.all()
+	
+
+	
 	# Swap out the files found to be substituted and rendered into the HTML.
 	#html = t.render({'loaded_files': audio_track_list})
+	#if(len(video_track_list) > 0):
+	#	print("There are new files.")
+	#	html = t.render({'loaded_files': new_files})
+	#else:
+	#	print("There are no files.")
 	html = t.render()
 
 	return HttpResponse(html)
@@ -168,7 +162,43 @@ def scan_for_new_audio_files():
 		if(database_file in loaded_files):
 			loaded_files.remove(database_file)
 
+	print("---------Done-------------------Scanning----------------")
 	return loaded_files
+
+
+def scan_for_new_video_files():
+	print("---------Scanning---for-----new------files----------------")
+	# Obtain a list of all media files in the media folder.
+	loaded_files = obtain_all_video_filenames()
+
+	# Obtain all objects from the database
+	video_track_list = VideoTrack.objects.all()
+
+	# If we have the same count or more files in the database, don't load any more.
+	if (len(video_track_list) >= len(loaded_files)):
+		return []
+
+	# If the database is empty, load all files in the media folder.
+	if (len(video_track_list) == 0):
+		print("No video tracks in the database")
+		return loaded_files
+
+	# Output all files in the media folder.
+	for video_file in loaded_files:
+		print("Video File")
+		print(video_file)
+
+	# Output all files in the database.
+	for track in video_track_list:
+		print("Video Track")
+		print(track.file_name)
+
+	# Iterate through all audio files in the database, if the filename matches
+	# a filename matched in the media folder, remove it from the list of 
+	# tracks to be added.
+	for database_file in video_track_list:
+		if(database_file in loaded_files):
+			loaded_files.remove(database_file)
 
 	print("---------Done-------------------Scanning----------------")
 	return loaded_files
@@ -212,9 +242,33 @@ def write_ID3_tag_information_to_database(current_audio_track):
 	return audio_track_object
 
 
+def write_video_information_to_database(current_video_track):
+	''' This function will obtain individual ID3 tags from an MP3 file and save it to the AudioTrack
+	Django Model if the value is non-null. '''
+	# Initialize all fields to an empty string.
+	video_title = ""
+	artist = ""
+	genre = ""
+	play_count = ""
+	rating = ""
+	year = ""
+	file_size = ""
+	file_name = ""
+
+	'''
+	
+
+	audio_track_object = VideoTrack(file_name = str(current_audio_track), song_title=str(song_title), artist=str(performer), album=str(album), genre=genre, year=year, track_number=track_number)
+
+	audio_track_object.save()
+
+	# Save all fields for this database row entry.
+	return audio_track_object
+	'''
+
 
 def obtain_all_audio_filenames():
-	''' This function will obtain a list of all files in the media folder. '''
+	''' This function will obtain a list of all audio files in the media folder. '''
 	print("----------------------------------------------------------------")
 	print("Obtaining all media file_names")
 	if(VERBOSE):
@@ -233,6 +287,34 @@ def obtain_all_audio_filenames():
 
 	return loaded_files, num_of_files
 
+def obtain_all_video_filenames():
+	''' This function will obtain a list of all video files in the media folder. '''
+	print("----------------------------------------------------------------")
+	print("Obtaining all video file_names")
+	if(VERBOSE):
+		print("\nMEDIA DIRECTORY: " + MEDIA_ROOT)
+
+	loaded_files = []
+	num_of_files = 0
+
+	# Iterate over all files found in the media directory. Count the total number of files.
+	for filename in os.listdir(os.getcwd() + "/media/"):
+		if(filename.endswith(".mp4") or filename.endswith(".ogg") or filename.endswith(".webm") or filename.endswith(".ogv") or filename.endswith(".mkv")):
+			num_of_files+=1
+			loaded_files.append(filename)
+
+			if(VERBOSE):
+				print(filename)
+
+	print("Loaded Video Files:")
+	print(loaded_files)
+	print("\n")
+
+	return loaded_files, num_of_files
+
+
+
+
 def clean(instr):
     return instr.translate(None, string.punctuation + '')
 
@@ -241,3 +323,7 @@ def parse_RSS_feed(url):
 
 
 	pass
+
+def getVideoMetadata(filename):
+	result = subprocess.Popen(["ffprobe", filename], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+	return result.stdout.readlines()
